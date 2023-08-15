322b253f9718d207015bac81ace7104fd4e0d3ee
Joseph Corral <447399989@student.sbhs.nsw.edu.au>
2023-08-15 12:50:01 +1000

    changed gameplay screen size

--- The Great Bug War/Gameplay Screen.Designer.vb
+++ The Great Bug War/Gameplay Screen.Designer.vb
@@ -22,9 +22,17 @@ Partial Class Gameplay_Screen
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.SuspendLayout()
+        '
+        'Gameplay_Screen
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(500, 700)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Gameplay_Screen"
         Me.Text = "Gameplay_Screen"
+        Me.ResumeLayout(False)
+
     End Sub
 End Class
